3e69bf04472e364850791038449e9b83c96c0319
Radhi Rasho <Radhi.h@outlook.com>
2024-10-16 18:10:42 +0000

    working on it

--- components/Render.tsx
+++ components/Render.tsx
@@ -1,56 +1,211 @@
 "use client";
 import { Html, OrbitControls, useProgress } from "@react-three/drei";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import {
+	Box3,
+	type BufferAttribute,
 	BufferGeometry,
 	CatmullRomCurve3,
 	Clock,
+	DataTexture,
+	FloatType,
+	Line,
 	LineBasicMaterial,
-	type Mesh,
+	MathUtils,
+	Mesh,
+	MeshBasicMaterial,
+	NearestFilter,
+	RGBFormat,
+	type Scene,
+	Vector2,
 	Vector3,
+	type WebGLProgramParametersWithUniforms,
 } from "three";
-import { ThreeMFLoader } from "three/examples/jsm/Addons.js";
+import { STLLoader } from "three/examples/jsm/Addons.js";
 
 type Props = {
 	position: [number, number, number];
 };
+
+type Uniform = {
+	uSpatialTexture: { value: DataTexture };
+	uTextureSize: { value: Vector2 };
+	uTime: { value: number };
+	uLengthRatio: { value: number };
+	uObjSize: { value: Vector3 };
+};
+
 function Fish(props: Props) {
 	const meshRef = useRef<Mesh>(null);
-	const fish = useLoader(ThreeMFLoader, "koifish.3mf");
+	const [oUs, setOUs] = useState<Uniform[]>([]);
 
-	// Animation state
+	function vertexShader(shader: WebGLProgramParametersWithUniforms) {
+		const vertexShader = `
+		uniform sampler2D uSpatialTexture;
+		uniform vec2 uTextureSize;
+		uniform float uTime;
+		uniform float uLengthRatio;
+		uniform vec3 uObjSize;
+
+		struct splineData {
+			vec3 point;
+			vec3 binormal;
+			vec3 normal;
+		};
+
+		splineData getSplineData(float t){
+			float step = 1. / uTextureSize.y;
+			float halfStep = step * 0.5;
+			splineData sd;
+			sd.point    = texture2D(uSpatialTexture, vec2(t, step * 0. + halfStep)).rgb;
+			sd.binormal = texture2D(uSpatialTexture, vec2(t, step * 1. + halfStep)).rgb;
+			sd.normal   = texture2D(uSpatialTexture, vec2(t, step * 2. + halfStep)).rgb;
+			return sd;
+		}
+  		${shader.vertexShader}
+		`;
+
+		return vertexShader;
+	}
+
+	useEffect(() => {
+		const loader = new STLLoader();
+		loader.load("fish.stl", (objGeom) => {
+			console.log(objGeom);
+			//objGeom.rotateX(-MathPI * 0.5);
+
+			// path
+			const baseVector = new Vector3(40, 0, 0);
+			const axis = new Vector3(0, 1, 0);
+			const cPts = [];
+			const cSegments = 6;
+			const cStep = (Math.PI * 2) / cSegments;
+			for (let i = 0; i < cSegments; i++) {
+				cPts.push(
+					new Vector3()
+						.copy(baseVector)
+						//.setLength(35 + (Math.random() - 0.5) * 5)
+						.applyAxisAngle(axis, cStep * i)
+						.setY(MathUtils.randFloat(-10, 10)),
+				);
+			}
+			const curve = new CatmullRomCurve3(cPts);
+			curve.closed = true;
+
+			console.log(curve);
+
+			const numPoints = 511;
+			const cPoints = curve.getSpacedPoints(numPoints);
+			const cObjects = curve.computeFrenetFrames(numPoints, true);
+			console.log(cObjects);
+			const pGeom = new BufferGeometry().setFromPoints(cPoints);
+			const pMat = new LineBasicMaterial({ color: "yellow" });
+			const pathLine = new Line(pGeom, pMat);
+			meshRef.current?.add(pathLine);
+
+			// data texture
+			const data = [];
+			for (const v of cPoints) {
+				data.push(v.x, v.y, v.z);
+			}
+			for (const v of cObjects.binormals) {
+				data.push(v.x, v.y, v.z);
+			}
+			for (const v of cObjects.normals) {
+				data.push(v.x, v.y, v.z);
+			}
+			for (const v of cObjects.tangents) {
+				data.push(v.x, v.y, v.z);
+			}
+
+			const dataArray = new Float32Array(data);
+
+			const tex = new DataTexture(
+				dataArray,
+				numPoints + 1,
+				4,
+				RGBFormat,
+				FloatType,
+			);
+			tex.magFilter = NearestFilter;
+			console.log(tex);
+
+			objGeom.center();
+			objGeom.rotateX(-Math.PI * 0.5);
+			objGeom.scale(0.5, 0.5, 0.5);
+			const objBox = new Box3().setFromBufferAttribute(
+				objGeom.getAttribute("position") as BufferAttribute,
+			);
+			const objSize = new Vector3();
+			objBox.getSize(objSize);
+			//objGeom.translate(0, 0, objBox.z);
+
+			const objUniforms = {
+				uSpatialTexture: { value: tex },
+				uTextureSize: { value: new Vector2(numPoints + 1, 4) },
+				uTime: { value: 0 },
+				uLengthRatio: { value: objSize.z / curve.getLength() }, // more or less real length along the path
+				uObjSize: { value: objSize }, // lenght
+			};
+			oUs.push(objUniforms);
+
+			const objMat = new MeshBasicMaterial({
+				color: 0xff6600,
+				wireframe: true,
+			});
+			objMat.onBeforeCompile = (shader) => {
+				shader.uniforms.uSpatialTexture = objUniforms.uSpatialTexture;
+				shader.uniforms.uTextureSize = objUniforms.uTextureSize;
+				shader.uniforms.uTime = objUniforms.uTime;
+				shader.uniforms.uLengthRatio = objUniforms.uLengthRatio;
+				shader.uniforms.uObjSize = objUniforms.uObjSize;
+
+				shader.vertexShader = vertexShader(shader);
+				shader.vertexShader = shader.vertexShader.replace(
+					"#include <begin_vertex>",
+					`#include <begin_vertex>
+
+					vec3 pos = position;
+
+					float wStep = 1. / uTextureSize.x;
+					float hWStep = wStep * 0.5;
+
+					float d = pos.z / uObjSize.z;
+					float t = fract((uTime * 0.1) + (d * uLengthRatio));
+					float numPrev = floor(t / wStep);
+					float numNext = numPrev + 1.;
+					//numNext = numNext > (uTextureSize.x - 1.) ? 0. : numNext;
+					float tPrev = numPrev * wStep + hWStep;
+					float tNext = numNext * wStep + hWStep;
+					//float tDiff = tNext - tPrev;
+					splineData splinePrev = getSplineData(tPrev);
+					splineData splineNext = getSplineData(tNext);
+
+					float f = (t - tPrev) / wStep;
+					vec3 P = mix(splinePrev.point, splineNext.point, f);
+					vec3 B = mix(splinePrev.binormal, splineNext.binormal, f);
+					vec3 N = mix(splinePrev.normal, splineNext.normal, f);
+
+					transformed = P + (N * pos.x) + (B * pos.y);`,
+				);
+				console.log(shader.vertexShader);
+			};
+			const obj = new Mesh(objGeom, objMat);
+			meshRef.current?.add(obj);
+		});
+	}, []);
+
 	const clock = new Clock();
 
 	useFrame(() => {
-		if (meshRef.current) {
-			const elapsedTime = clock.getElapsedTime();
-			const radius = 5;
-			const speed = 0.5;
-
-			// Calculate the new position
-			const x = radius * Math.cos(speed * elapsedTime);
-			const z = radius * Math.sin(speed * elapsedTime);
-
-			// Calculate the direction vector
-			const nextX = radius * Math.cos(speed * (elapsedTime + 0.01));
-			const nextZ = radius * Math.sin(speed * (elapsedTime + 0.01));
-			const direction = new Vector3(nextX - x, 0, nextZ - z).normalize();
-
-			// Update the position
-			meshRef.current.position.set(x, 0, z);
-
-			// Update the rotation to face the direction of movement
-			meshRef.current.lookAt(x + direction.x, 0, z + direction.z);
+		const t = clock.getElapsedTime();
+		for (const ou of oUs) {
+			ou.uTime.value = t;
 		}
 	});
 
-	return (
-		<mesh ref={meshRef} {...props} dispose={null}>
-			<primitive object={fish} scale={0.1} />
-			<meshBasicMaterial wireframe={true} />
-		</mesh>
-	);
+	return <mesh ref={meshRef} {...props} />;
 }
 
 function Loader() {
@@ -66,7 +221,7 @@ export default function Render() {
 			className="h-full w-full"
 		>
 			<Suspense fallback={<Loader />}>
-				<color attach={"background"} args={["white"]} />
+				<color attach={"background"} args={["black"]} />
 				<ambientLight intensity={0.1} />
 				<directionalLight position={[10, 10, 10]} intensity={0.5} />
 				<directionalLight
